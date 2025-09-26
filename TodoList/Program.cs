c6fb1a6a3b248fa34acb9eafcb4a8462a3980948
Артем Бешкеев <11071980ir@gmail.com>
2025-09-27 00:27:20 +0300

    выполнил проверку

---
+++ TodoList/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+Console.Write("Введите имя: ");
+string name = Console.ReadLine();
+
+Console.Write("Введите фамилию: ");
+string surname = Console.ReadLine();
+
+Console.Write("Введите год рождения: ");
+string birthYearString = Console.ReadLine();
+
+int birthYear = int.Parse(birthYearString);
+int currentYear = DateTime.Now.Year;
+int age = currentYear - birthYear;
+
+Console.WriteLine($"Добавлен пользователь {name} {surname}, возраст - {age}");
